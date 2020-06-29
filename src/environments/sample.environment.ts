f7b62dd0b787bac4e4150f90ba7cd51351755921
Rob Pataki <robert@robertpataki.com>
2020-06-29 15:26:15 +0100

    Add environment file samples

---
+++ src/environments/sample.environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false
+};
+
+export const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  databaseUrl: 'YOUR_DB_URL',
+  authSignInUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword',
+  authResetPasswordUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode',
+  authRefreshTokenUrl: 'https://securetoken.googleapis.com/v1/token'
+};
